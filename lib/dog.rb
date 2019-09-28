6b58c3bb8002d99904a31ecd1f71e8c88ad5bd4e
Randy Taylor <randyisbusy@gmail.com>
2019-09-28 06:27:38 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,80 @@
+require 'pry'
+class Dog
+  attr_accessor :name, :breed
+  attr_reader :id
+  def initialize(id: nil, name:, breed: )
+    @id = id
+    @name = name
+    @breed = breed
+  end
+  def self.create_table
+    sql = <<-YOU
+    CREATE TABLE IF NOT EXISTS dogs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT
+    )
+    YOU
+    DB[:conn].execute(sql)
+  end
+  def self.drop_table
+    sql = <<-LOVE
+    DROP TABLE dogs
+    LOVE
+    DB[:conn].execute(sql)
+  end
+  def save
+    if self.id
+     self.update
+   else
+     sql = <<-SQL
+       INSERT INTO dogs (name, breed)
+       VALUES (?, ?)
+     SQL
+
+     DB[:conn].execute(sql, self.name, self.breed)
+     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+     dog = DB[:conn].execute("SELECT * FROM dogs WHERE id = last_insert_rowid()")
+     new_dog = Dog.new(id: dog[0], name: dog[1], breed: dog[2])
+   end
+  end
+  def update
+    sql = "UPDATE dogs SET name = ?, album = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.album, self.id)
+  end
+
+  def self.create( name:, breed:)
+    new_dog = self.new({name: name, breed: breed})
+    new_dog.save
+    new_dog
+    end
+  def self.new_from_db(row)
+    new_doggy = self.new(id: row[0], name: row[1], breed: row[2])
+
+    new_doggy
+  end
+  def self.find_by_id(i)
+      sql = <<-RANDY
+      SELECT * FROM dogs WHERE id = ?
+      RANDY
+      dog = DB[:conn].execute(sql, i)[0]
+      doggy = self.new_from_db(dog)
+
+  end
+
+  def self.find_or_create_by(arg)
+    name = arg[:name]
+    breed = arg[:breed]
+    sql = <<-NEWNEW
+    SELECT * FROM dogs WHERE name = ?
+    NEWNEW
+    dog = DB[:conn].execute(sql, name)
+
+    if dog
+      id = dog[0][0]
+      self.find_by_id(id)
+    else
+      self.create(arg[:name], arg[:breed])
+  end
+end
+end
